6479be6b093f5e17e075505ed1db3b3122c56911
Joseph Soria <josephsoria6@gmail.com>
2021-04-04 22:45:04 -0500

    Added get routes for each endpoint

--- server.js
+++ server.js
@@ -1,38 +1,46 @@
-const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
-const path = require("path");
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+const path = require(`path`);
+
+
+const PORT = process.env.PORT || 3000;
+
+const db = require(`./models`);
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger(`dev`));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(`public`));
 
-const databaseUrl = "notetaker";
-const collections = ["notes"];
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, { useNewUrlParser: true });
 
-const db = mongojs(databaseUrl, collections);
 
-db.on("error", error => {
-  console.log("Database Error:", error);
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname + `/public/html/index.html`));
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/index.html"));
+app.get(`/exercise`, (req, res) => {
+  res.sendFile(path.join(__dirname + `/public/html/exercise.html`));
 });
 
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/exercise.html"));
+app.get(`/stats`, (req, res) => {
+  db.Workout.find({}).populate(`exercises`)
+  .then(response => {
+    console.log(response)
+  })
+  res.sendFile(path.join(__dirname + `/public/html/stats.html`));
 });
 
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/html/stats.html"));
+app.get(`/api/workouts/range`, (req,res) => {
+
+})
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
 
-app.listen(3000, () => {
-  console.log("App running on port 3000!");
-});
