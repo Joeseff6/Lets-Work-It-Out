d976a2fc9b93cc252cd5a263648b0dca88010e52
Joseph Soria <josephsoria6@gmail.com>
2021-04-06 22:21:01 -0500

    Nested exercises into WorkoutSchema

--- models/Workout.js
+++ models/Workout.js
@@ -4,14 +4,39 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        required: true,
     },
     exercises: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise",
+        type: {
+            type: String,
+            trim: true,
         },
-    ],
+        name: {
+            type: String,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            min: 0,
+            max: 60,
+        },
+        weight: {
+            type: Number,
+            min: 2.5,
+        },
+        reps: {
+            type: Number,
+            min: 1,
+        },
+        sets: {
+            type: Number,
+            min: 1,
+        },
+        distance: {
+            type: Number,
+        },
+    },
+],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema)
